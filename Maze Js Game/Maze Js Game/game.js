e1ae575b84f3fe38b026aa392050052e2e629ab3
MasriAmani <amanimasri92@gmail.com>
2021-09-03 09:46:50 +0300

    Fixing the code

--- Maze Js Game/Maze Js Game/game.js
+++ Maze Js Game/Maze Js Game/game.js
@@ -1,37 +1,75 @@
 window.onload = function(){
 	
-	console.log("jhjhjhj");
-	
-	var elements = document.getElementsByClassName("boundary");
-           for (var i=0 ; i< elements.length ; i++) {
-			   
-          elements[i].addEventListener("mouseover", func);
-		      
-       function func()
-              {         
-           for (var i=0 ; i< elements.length ; i++) {			  
-           elements[i].setAttribute("style", "background-color:red;");
-		   }
-		   var element = document.getElementByID("status").innerHTML = "YOU LOST";}
-		  
-		    	   }
-		   
-		   
-		   
-		 var element1 = document.getElementByID("start").addEventListener("click", func1);
-		    func1()
-		         {   
-           			 
-            for (var i=0 ; i< elements.length ; i++) {			  
-            elements[i].setAttribute("style", "background-color:#eeeeee;");
-		   }
-		    var element = document.getElementByID("status").innerHTML = "Begin by moving your mouse over the S";
-		   }
-		 
-	
+	var start = false;
+	var inside = false;
+	var score =0;
+	var elements1 = document.getElementsByClassName("boundary example");
+   elements1[0].innerHTML ="Restart <br><br> Score :  "+score ;
 
 	
-	
+	var elements = document.getElementsByClassName("boundary");
+           for (var i=0 ; i< (elements.length)-1 ; i++) {
+			   
+		   elements[i].addEventListener("mouseover", func);}
+		      
+       function func()
+              {   
+			  if (start){
+           for (var i=0 ; i< (elements.length)-1 ; i++) {			  
+           elements[i].setAttribute("style", "background-color:red;");
+		   }
+		   var element = document.getElementById("status").innerHTML = "YOU LOST";
+		   start = false;
+		   score -= 10;
+		   elements1[0].innerHTML ="Restart <br><br> Score :  "+score ;}}
+		  
+		    	   
+		   
+		   
+		   
+		 var element1 = document.getElementById("start").addEventListener("click", func1);
+		    function func1()
+		         {   
+           			 
+            for (var i=0 ; i< (elements.length)-1 ; i++) {			  
+            elements[i].setAttribute("style", "background-color:#eeeeee;");
+		   }
+		    var element = document.getElementById("status").innerHTML = 'Begin by moving your mouse over the "S".';
+			start =true;
+			inside = true;
+		   }
+		 
+		 
+		 var element2 = document.getElementById("end").addEventListener("mouseover", func2);
+		    function func2()
+		         {   
+           			if (start && inside) {
+           
+		    var element = document.getElementById("status").innerHTML = "YOU WON ";
+			score += 5 ;
+			elements1[0].innerHTML ="Restart <br><br> Score :  "+score ;
+			start = false;
+		   }
+				 }
+	    
+		
+		   var element3 = document.getElementById("game").addEventListener("mouseleave", func3);
+		  
+		
+		   function func3()
+		         {   
+           			inside=false;
+				
+				 }
+		 
+	    
+	 elements1[0].addEventListener("click", func4);
+	 function func4() {
+		 
+       score = 0;
+	  elements1[0].innerHTML ="Restart <br><br> Score :  "+score ;
+	 }
+
 	
 	
 	
