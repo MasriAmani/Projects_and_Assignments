1ffc8517dd53bb33d8798942b31557a0eac67865
MasriAmani <amanimasri92@gmail.com>
2021-09-08 19:10:11 +0300

    Updated the database (sql) file

--- Ecommerce/Database/ecommercedb.sql
+++ Ecommerce/Database/ecommercedb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 08, 2021 at 01:55 PM
+-- Generation Time: Sep 08, 2021 at 06:09 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -37,7 +37,9 @@ CREATE TABLE `customer_order` (
 --
 
 INSERT INTO `customer_order` (`customer_id`, `started`) VALUES
-(11, 1);
+(7, 0),
+(11, 0),
+(12, 0);
 
 -- --------------------------------------------------------
 
@@ -56,7 +58,11 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `customer_id`, `payed`) VALUES
-(11, 11, 0);
+(11, 11, 1),
+(12, 12, 1),
+(13, 12, 1),
+(14, 7, 1),
+(15, 7, 1);
 
 -- --------------------------------------------------------
 
@@ -79,12 +85,14 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `image`, `quantity`, `price`, `store_id`) VALUES
-(12, 'Blue Jeans', 'New casual blue jeans, very comfortable and trendy.', 'product-02.jpg', 10, 40, 6),
-(13, 'Converse R', 'Red converse to complete your ultimate look.', 'product-04.jpg', 5, 80, 6),
+(12, 'Blue Jeans2', 'New casual blue jeans, very comfortable and trendy.', 'product-02.jpg', 3, 40, 6),
+(13, 'Converse R', 'Red converse to complete your ultimate look.', 'product-04.jpg', 3, 80, 6),
 (14, 'Pink Purse', 'Leather curve shoulder bag for women. ', 'product-06.jpg', 13, 55, 6),
 (15, 'Blue Jacket', 'Winter blue trendy jacket suitable for everyday.', 'product-03.jpg', 12, 123, 6),
-(16, 'Blue Jeans', 'New casual blue jeans, very comfortable and trendy.', 'product-02.jpg', 6, 26, 7),
-(17, 'Blue Jacket', 'Winter blue trendy jacket suitable for everyday.', 'product-03.jpg', 10, 48, 8);
+(16, 'Blue Jeans', 'New casual blue jeans, very comfortable and trendy.', 'product-02.jpg', 2, 26, 7),
+(17, 'Blue Jacket2', 'Winter blue trendy jacket suitable for everyday.', 'product-03.jpg', 10, 48, 8),
+(18, 'Blue Purse', 'Trendy cross shoulder blue purse for everyday.', 'product-05.jpg', 11, 29, 9),
+(20, 'Converse R 2', 'Red converse to complete your ultimate look.', 'product-04.jpg', 13, 156, 9);
 
 -- --------------------------------------------------------
 
@@ -107,7 +115,14 @@ INSERT INTO `products_orders` (`id`, `order_id`, `product_id`, `quantity`) VALUE
 (12, 11, 12, 5),
 (13, 11, 13, 2),
 (14, 11, 14, 1),
-(15, 11, 15, 8);
+(15, 11, 15, 8),
+(16, 12, 13, 1),
+(17, 13, 14, 10),
+(18, 13, 17, 3),
+(19, 14, 13, 1),
+(20, 14, 12, 7),
+(21, 14, 16, 4),
+(22, 15, 13, 1);
 
 -- --------------------------------------------------------
 
@@ -130,7 +145,8 @@ CREATE TABLE `stores` (
 INSERT INTO `stores` (`id`, `name`, `phone`, `city`, `owner_id`) VALUES
 (6, 'Zara', '+96170289518', 'Beirut', 5),
 (7, 'Berskha', '+96170289518', 'Jbeil', 6),
-(8, 'Eternity', '+9613345158', 'Jbeil', 10);
+(8, 'Eternity', '+9613345158', 'Jbeil', 10),
+(9, 'Bella', '+9613345158', 'Beirut', 13);
 
 -- --------------------------------------------------------
 
@@ -160,7 +176,9 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `gender`, `phone_number`, 
 (7, 'Lana', 'wehbe', 1, '+96170258956', 'lana@gmail.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'Nabatieh', 0),
 (8, 'Mohamad', 'Masri', 0, 'mnmn', 'mohamad@gmail.com', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'Nabatieh', 0),
 (10, 'Sara', 'Mohsen', 1, '+96171365800', 'sara@gmail.com', '8588310a98676af6e22563c1559e1ae20f85950792bdcd0c8f334867c54581cd', 'Beirut', 1),
-(11, 'Rania', 'Yamout', 0, '+96170258956', 'rania@gmail.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'Jounieh', 0);
+(11, 'Rania', 'Yamout', 0, '+96170258956', 'rania@gmail.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'Jounieh', 0),
+(12, 'Mohamad', 'Masri', 0, '+96170258956', 'm@gmail.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'Beirut', 0),
+(13, 'Yara', 'Masri', 1, '+96170258956', 'yara@gmail.com', '481f6cc0511143ccdd7e2d1b1b94faf0a700a8b49cd13922a70b5ae28acaa8c5', 'Tripoli', 1);
 
 --
 -- Indexes for dumped tables
@@ -212,37 +230,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `customer_order`
 --
 ALTER TABLE `customer_order`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `products_orders`
 --
 ALTER TABLE `products_orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `stores`
 --
 ALTER TABLE `stores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
